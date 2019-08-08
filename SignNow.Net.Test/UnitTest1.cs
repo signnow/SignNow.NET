f2906ac2eb8dd3040391779ab585086a2da44395
bilokon.ivan <bilokon.ivan@pdffiller.team>
2019-08-08 11:35:57 +0300

    [MSAPI-53] Created project and unit test project

---
+++ SignNow.Net.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SignNow.Net.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SignNow.Net/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SignNow.Net
+{
+    public class Class1
+    {
+    }
+}
