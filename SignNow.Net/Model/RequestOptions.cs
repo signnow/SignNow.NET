0d218de93419c13c9dcddde2b058c762a19131de
AlexNDRmac <alexndr.mac@gmail.com>
2019-08-13 17:00:00 +0300

    MSAPI-57 | Implement client, improve Request Opt

--- SignNow.Net/Model/RequestOptions.cs
+++ SignNow.Net/Model/RequestOptions.cs
@@ -1,6 +1,15 @@
+using System.Collections.Generic;
+
 namespace SignNow.Net.Model
 {
     public class RequestOptions
     {
+        public string Method { get; set; }
+
+        public string Uri { get; set; }
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public string Content { get; set; } = string.Empty;
     }
 }
--- SignNow.Net/SignNowClient.cs
+++ SignNow.Net/SignNowClient.cs
@@ -1,20 +1,88 @@
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SignNow.Net.Interface;
 using SignNow.Net.Model;
 
 namespace SignNow.Net
 {
-    public class SignNowClient : ISignNowClient
+    class SignNowClient : HttpClient, ISignNowClient
     {
-        public SignNowClient()
+        private System.Net.Http.HttpClient HttpClient { get; }
+
+        /// <summary>
+        /// Initialize a new instance of SignNow Client
+        /// </summary>
+        /// <param name="httpClient">
+        /// The <see cref="ISignNowClient"/> client to use. If <c>null</c>, an HTTP client will be
+        /// created with default parameters.
+        /// </param>
+        public SignNowClient(HttpClient httpClient = null)
         {
+            this.HttpClient = httpClient ?? MakeDefaultHttpClient();
         }
 
-        Task<TResponse> ISignNowClient.RequestAsync<TResponse>(RequestOptions requestOptions, CancellationToken cancellationToken)
+        public async Task<TResponse> RequestAsync<TResponse>(RequestOptions requestOptions, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var request = CreateHttpRequest(requestOptions);
+
+            var response = await this.HttpClient.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+            return ProcessResponse<TResponse>(response);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="System.Net.Http.HttpClient"/> class
+        /// with default parameters.
+        /// </summary>
+        /// <returns>The new instance of the <see cref="System.Net.Http.HttpClient"/> class.</returns>
+        private HttpClient MakeDefaultHttpClient()
+        {
+            return new HttpClient();
+        }
+
+        private HttpRequestMessage CreateHttpRequest(RequestOptions requestOptions)
+        {
+            var httpMethod = new HttpMethod(requestOptions.Method);
+            var requestMessage = new HttpRequestMessage(httpMethod, requestOptions.Uri);
+
+
+            foreach (var header in requestOptions.Headers)
+            {
+                requestMessage.Headers.Add(header.Key, header.Value);
+            }
+
+            requestMessage.Content = this.CreateContent(httpMethod, requestOptions.Content);
+
+            return requestMessage;
+        }
+
+        private HttpContent CreateContent(HttpMethod method, string content)
+        {
+            if (method == HttpMethod.Post)
+            {
+                return new StringContent(content, System.Text.Encoding.UTF8, "application/json");
+            }
+
+            return null;
+        }
+
+        private TResponse ProcessResponse<TResponse>(HttpResponseMessage response)
+        {
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new NotImplementedException();
+            }
+
+            TResponse responseObj;
+
+            var content = response.Content.ReadAsStringAsync().Result;
+            responseObj = JsonConvert.SerializeObject(content);
+
+            return responseObj;
         }
     }
 }
