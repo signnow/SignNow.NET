b415e14783d8e4e2e33e8350b26481f0983225bd
Khodan Andrii <khodan.andrii@pdffiller.team>
2019-08-15 17:41:04 +0300

    MSAPI-65 | imploment method  GetTokenAsync(string login, string password, Scope scope, CancellationToken cancellationToken = default)

--- SignNow.Net/Model/Token.cs
+++ SignNow.Net/Model/Token.cs
@@ -1,6 +1,21 @@
+using Newtonsoft.Json;
+
 namespace SignNow.Net.Model
 {
     public class Token
     {
+        [JsonProperty(PropertyName = "expires_in")]
+        public int ExpiresIn { get; set; }
+        [JsonProperty(PropertyName = "token_type")]
+        public string TokenType { get; set; }
+        [JsonProperty(PropertyName = "access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty(PropertyName = "refresh_token")]
+        public string RefreshToken { get; set; }
+        [JsonProperty(PropertyName = "scope")]
+        public string Scope { get; set; }
+        [JsonProperty(PropertyName = "last_login")]
+        public int LastLogin { get; set; }
     }
 }
+
--- SignNow.Net/OAuth2Service.cs
+++ SignNow.Net/OAuth2Service.cs
@@ -1,8 +1,13 @@
+using Newtonsoft.Json;
 using SignNow.Net._Internal.Constants;
 using SignNow.Net.Interface;
 using SignNow.Net.Model;
 using SignNow.Net.Service;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,26 +15,53 @@ namespace SignNow.Net
 {
     public class OAuth2Service : WebClientBase, IOAuth2Service
     {
+        private string _clientId { get; set; }
+
+        private string _clientSecret { get; set; }
+
+        private Uri _apiBaseUrl { get; set; }
+
         public OAuth2Service(string clientId, string clientSecret) : this(ApiUrl.ApiBaseUrl, clientId, clientSecret)
         {
-
+            _clientId = clientId;
+            _clientSecret = clientSecret;
         }
         public OAuth2Service(Uri apiBaseUrl, string clientId, string clientSecret) : base(apiBaseUrl)
         {
-
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _apiBaseUrl = apiBaseUrl;
         }
         protected OAuth2Service(Uri apiBaseUrl, string clientId, string clientSecret, ISignNowClient signNowClient) : base(apiBaseUrl, signNowClient)
         {
-
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _apiBaseUrl = apiBaseUrl;
         }
         public async Task<Uri> GetAuthorizationUrlAsync(Scope scope, CancellationToken cancellationToken = default(CancellationToken))
         {
             throw new NotImplementedException();
         }
 
-        public async Task<Token> GetTokenAsync(string login, string password, Scope scope, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<Token> GetTokenAsync(string login, string password, Scope scope, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var client = new HttpClient { BaseAddress = _apiBaseUrl };
+            var jsonData = new Dictionary<string, string>
+                {
+                    { "client_id", _clientId },
+                    { "client_secret", _clientSecret},
+                    { "username", login},
+                    { "username", password},
+                    { "grant_type", "password"},
+                    { "scope", scope.ToString()}
+                };
+            var content = new FormUrlEncodedContent(jsonData);
+
+            var response = await client.PostAsync(
+                "/oauth2/token", content).ConfigureAwait(false);
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<Token>(responseString);
         }
 
         public async Task<Token> GetTokenAsync(string code, Scope scope, CancellationToken cancellationToken = default(CancellationToken))
