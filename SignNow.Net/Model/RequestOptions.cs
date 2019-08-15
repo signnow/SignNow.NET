72d4b7ff09bc40064d04dc25b765e789daf4b99e
AlexNDRmac <alexndr.mac@gmail.com>
2019-08-15 16:55:54 +0300

    MSAPI-57 | Modify Request Options

--- SignNow.Net/Model/RequestOptions.cs
+++ SignNow.Net/Model/RequestOptions.cs
@@ -1,15 +1,56 @@
+using System;
 using System.Collections.Generic;
 
 namespace SignNow.Net.Model
 {
-    public class RequestOptions
+    public abstract class RequestOptions
     {
-        public string Method { get; set; }
+        public object Content { get; set; }
 
-        public string Uri { get; set; }
+        public Uri RequestUrl { get; set; }
 
-        public Dictionary<string, string> Headers { get; set; }
+        public abstract string HttpMethod { get; }
 
-        public string Content { get; set; } = string.Empty;
+        Token Token { get; set; }
+    }
+
+    public class GetHttpRequestOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Get.ToString();
+    }
+
+    public class PostHttpRequesOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Post.ToString();
+
+        public PostHttpRequesOptions(object content)
+        {
+            Content = content;
+        }
+    }
+
+    public class PutHttpRequesOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Put.ToString();
+
+        public PutHttpRequesOptions(object content)
+        {
+            Content = content;
+        }
+    }
+
+    public class DeleteHttpRequesOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Delete.ToString();
+    }
+
+    public class HeadHttpRequestOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Head.ToString();
+    }
+
+    public class OptionsHttpRequestOptions : RequestOptions
+    {
+        public override string HttpMethod => System.Net.Http.HttpMethod.Options.ToString();
     }
 }
