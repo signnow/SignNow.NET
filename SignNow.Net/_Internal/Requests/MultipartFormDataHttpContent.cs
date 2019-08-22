36940710166347d8b52bf6150b27958dcfb64dae
AlexNDRmac <alexndr.mac@gmail.com>
2019-08-22 16:11:32 +0300

    MSAPI-69 | add Multipart form data Content for Stream

---
+++ SignNow.Net/_Internal/Requests/MultipartFormDataHttpContent.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Net.Http;
+using SignNow.Net.Interfaces;
+using System.Net.Http.Headers;
+
+namespace SignNow.Net._Internal.Requests
+{
+    /// <summary>
+    /// A container for content encoded using <c>multipart/form-data</c> MIME type.
+    /// </summary>
+    internal class MultipartFormDataHttpContent : IContent
+    {
+        private Stream streamContent;
+
+        public MultipartFormDataHttpContent(Stream content)
+        {
+            this.streamContent = content;
+        }
+
+        public HttpContent GetHttpContent()
+        {
+            return CreateStreamContent(this.streamContent);
+        }
+
+        private StreamContent CreateStreamContent(Stream content)
+        {
+            var _streamContent = new StreamContent(content);
+
+            _streamContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "file"
+            };
+
+            return _streamContent;
+        }
+    }
+}
