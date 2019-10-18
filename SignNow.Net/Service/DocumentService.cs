4c332307f2150dcb760eaeab94c482e672bdd63b
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-10-18 17:09:25 +0300

    MSAPI-71 | Add document id validation for Delete

--- SignNow.Net/Service/DocumentService.cs
+++ SignNow.Net/Service/DocumentService.cs
@@ -6,11 +6,19 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using SignNow.Net.Internal.Requests;
+using System.Text.RegularExpressions;
 
 namespace SignNow.Net.Service
 {
     public class DocumentService : AuthorizedWebClientBase, IDocumentService
     {
+        /// <summary>
+        /// Pattern for Document Identyty
+        /// Allowed format must be: 40-chars
+        /// Allowed chars: digist, symbols a-z A-Z, underscore
+        /// </summary>
+        private static string documentIdPattern = @"^[a-zA-Z0-9_]{40,40}$";
+
         public DocumentService(Token token) : this(ApiUrl.ApiBaseUrl, token)
         {
 
@@ -40,6 +48,8 @@ namespace SignNow.Net.Service
         /// <inheritdoc />
         public async Task DeleteDocumentAsync(string documentId, CancellationToken cancellationToken = default)
         {
+            ValidateDocumentId(documentId);
+
             var requestedDocument = "/document/" + documentId;
 
             var requestOptions = new DeleteHttpRequestOptions
@@ -73,6 +83,24 @@ namespace SignNow.Net.Service
                 Token = this.Token
             };
             return await SignNowClient.RequestAsync<UploadDocumentResponse>(requestOptions, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Validates Document ID
+        /// </summary>
+        /// <param name="documentId">Identity of the document</param>
+        private void ValidateDocumentId(string documentId)
+        {
+            var regex = new Regex(documentIdPattern);
+
+            if (!regex.IsMatch(documentId))
+            {
+                throw new ArgumentException(
+                    "Wrong Document Id <" +
+                    documentId +
+                    ">. Allowed format should contains chars only [a-zA-Z0-9_] and lenght should be 40 symbols."
+                    );
+            }
         }
     }
 }
