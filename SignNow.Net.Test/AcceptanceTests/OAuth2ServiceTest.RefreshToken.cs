ac7fb5725b85dc3375c4271cdcd7ead63d23a006
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-04 16:00:52 +0200

    MSAPI-83 | Simplify test

--- SignNow.Net.Test/AcceptanceTests/OAuth2ServiceTest.RefreshToken.cs
+++ SignNow.Net.Test/AcceptanceTests/OAuth2ServiceTest.RefreshToken.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SignNow.Net.Model;
 using SignNow.Net.Test;
@@ -11,21 +10,17 @@ namespace AcceptanceTests
         [TestMethod]
         public void Should_Refresh_Token()
         {
-            var tokenTask = authObjectParam2.GetTokenAsync(userCredentials.Login, userCredentials.Password, Scope.All);
-
-            Task.WaitAll(tokenTask);
-
-            var token = tokenTask.Result;
+            var token = authObjectParam2.
+                GetTokenAsync(userCredentials.Login, userCredentials.Password, Scope.All).
+                Result;
 
             Assert.IsNotNull(token, "Token is null");
             Assert.IsFalse(String.IsNullOrEmpty(token.AccessToken));
             Assert.IsFalse(String.IsNullOrEmpty(token.RefreshToken));
 
-            var refreshTask = authObjectParam2.RefreshTokenAsync(token);
-
-            Task.WaitAll(refreshTask);
-
-            var freshToken = refreshTask.Result;
+            var freshToken = authObjectParam2.
+                RefreshTokenAsync(token).
+                Result;
 
             Assert.IsNotNull(freshToken, "Token is null");
             Assert.AreNotSame(token.AccessToken, freshToken.AccessToken, "Token is not refreshed");
