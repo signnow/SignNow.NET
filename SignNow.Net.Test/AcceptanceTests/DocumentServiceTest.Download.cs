3bce4b0bfa1aca6ab77701775b1fa0361e3a05b4
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-11 15:18:57 +0200

    MSAPI-80 | Rework Document Tests

--- SignNow.Net.Test/AcceptanceTests/DocumentServiceTest.Download.cs
+++ SignNow.Net.Test/AcceptanceTests/DocumentServiceTest.Download.cs
@@ -1,15 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SignNow.Net;
-using SignNow.Net.Interfaces;
-using SignNow.Net.Model;
 using SignNow.Net.Test;
-using SignNow.Net.Test.SignNow;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace AcceptanceTests
 {
@@ -18,16 +8,14 @@ namespace AcceptanceTests
         [TestMethod]
         public void DownloadDocumentAsPDF()
         {
-            var testDocumentId = UploadTestDocument(PdfFilePath);
+            DocumentId = UploadTestDocument(PdfFilePath);
 
-            var downloadResponse = docService.DownloadDocumentAsync(testDocumentId).Result;
+            var downloadResponse = docService.DownloadDocumentAsync(DocumentId).Result;
 
             Assert.IsTrue(
                 downloadResponse.CanRead,
                 "Download document result has an error, status code is not Successful"
                 );
-
-            DeleteDocument(testDocumentId);
         }
     }
 }
--- SignNow.Net/_Internal/Helpers/HttpContentAdapter.cs
+++ SignNow.Net/_Internal/Helpers/HttpContentAdapter.cs
@@ -9,19 +9,22 @@ namespace SignNow.Net.Internal.Helpers
     class HttpContentToObjectAdapter<TObject> : IHttpContentAdapter<TObject>
     {
         readonly IHttpContentAdapter<string> contentToStringAdapter;
+
         public HttpContentToObjectAdapter(IHttpContentAdapter<string> contentToStringAdapter)
         {
             this.contentToStringAdapter = contentToStringAdapter;
         }
+
         public async Task<TObject> Adapt(HttpContent content)
         {
             var stringContent = await contentToStringAdapter.Adapt(content).ConfigureAwait(false);
+
             return JsonConvert.DeserializeObject<TObject>(stringContent);
         }
     }
+
     class HttpContentToStringAdapter : IHttpContentAdapter<string>
     {
-
         public async Task<string> Adapt(HttpContent content)
         {
             return await content.ReadAsStringAsync().ConfigureAwait(false);
@@ -30,7 +33,6 @@ namespace SignNow.Net.Internal.Helpers
 
     class HttpContentToStreamAdapter : IHttpContentAdapter<Stream>
     {
-
         public async Task<Stream> Adapt(HttpContent content)
         {
             return await content.ReadAsStreamAsync().ConfigureAwait(false);
