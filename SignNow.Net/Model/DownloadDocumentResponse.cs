bc013ed310999c3747cbdf833e9afa4a8fc24a78
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-12 17:12:31 +0200

    MSAPI-80 | Add Model for download response

---
+++ SignNow.Net/Model/DownloadDocumentResponse.cs
@@ -0,0 +1,27 @@
+using System.IO;
+
+namespace SignNow.Net.Model
+{
+    public class DownloadDocumentResponse
+    {
+        /// <summary>
+        /// File name with extension.
+        /// </summary>
+        public string Filename { get; set; }
+
+        /// <summary>
+        /// File content type.
+        /// </summary>
+        public string MediaType { get; set; }
+
+        /// <summary>
+        /// File length in bytes
+        /// </summary>
+        public long Length { get; set; }
+
+        /// <summary>
+        /// File contents.
+        /// </summary>
+        public Stream Document { get; set; }
+    }
+}
