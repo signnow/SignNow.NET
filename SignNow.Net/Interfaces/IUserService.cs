4cfb326c39d38129ab5006a044fead67da85b2ff
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-12-30 17:52:38 +0200

    MSAPI-94 | Add User Interface

---
+++ SignNow.Net/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+using System.Threading.Tasks;
+using SignNow.Net.Model;
+
+namespace SignNow.Net.Interfaces
+{
+    /// <summary>
+    /// Represents a user resource
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Retrieve current user`s resource
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<User> GetCurrentUserAsync(CancellationToken cancellationToken = default);
+    }
+}
