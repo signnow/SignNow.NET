bc519221baa2dbeb9f866f4fafb51f7b5ea30376
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-12-31 09:59:42 +0200

    MSAPI-94 | Add custom json converter

---
+++ SignNow.Net/Model/Converters/StringToBoolJsonConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using Newtonsoft.Json;
+
+namespace SignNow.Net.Model.Converters
+{
+    internal class StringToBoolJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            switch (reader.Value.ToString().ToLower().Trim())
+            {
+                case "true":
+                case "1":
+                    return true;
+                case "false":
+                case "0":
+                    return false;
+                default:
+                    return new JsonSerializer().Deserialize(reader, objectType);
+            }
+
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool);
+        }
+    }
+}
