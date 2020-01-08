e382bc7716dfbad5b57b32c823d5e70dd083204d
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-08 14:29:20 +0200

    MSAPI-93 | Add User service to context

--- SignNow.Net/Interfaces/ISignNowContext.cs
+++ SignNow.Net/Interfaces/ISignNowContext.cs
@@ -3,5 +3,7 @@ namespace SignNow.Net.Interfaces
     public interface ISignNowContext
     {
         IDocumentService Documents { get; }
+
+        IUserService User { get; }
     }
 }
--- SignNow.Net/Service/AuthorizedWebClientBase.cs
+++ SignNow.Net/Service/AuthorizedWebClientBase.cs
@@ -7,10 +7,11 @@ namespace SignNow.Net.Service
     public class AuthorizedWebClientBase : WebClientBase
     {
         protected Token Token { get; set; }
+
         public AuthorizedWebClientBase(Uri apiBaseUrl, Token token) : this(apiBaseUrl, token, null)
         {
-
         }
+
         protected AuthorizedWebClientBase(Uri apiBaseUrl, Token token, ISignNowClient signNowClient) : base(apiBaseUrl, signNowClient)
         {
             Token = token;
