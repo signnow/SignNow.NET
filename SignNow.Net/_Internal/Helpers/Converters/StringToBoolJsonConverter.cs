bfabc193c2c8895dc39c8462d031648c3aead90e
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-10 17:03:26 +0200

    MSAPI-96 | Improve code style for converter

--- SignNow.Net/_Internal/Helpers/Converters/StringToBoolJsonConverter.cs
+++ SignNow.Net/_Internal/Helpers/Converters/StringToBoolJsonConverter.cs
@@ -1,32 +1,38 @@
 using System;
+using System.Globalization;
 using Newtonsoft.Json;
 
 namespace SignNow.Net.Internal.Helpers.Converters
 {
+    /// <summary>
+    /// Converts <see cref="String"/> to <see cref="Boolean"/> to and from JSON.
+    /// </summary>
     internal class StringToBoolJsonConverter : JsonConverter
     {
+        /// <inheritdoc cref="JsonConverter.WriteJson(JsonWriter, object, JsonSerializer)" />
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new NotSupportedException();
         }
 
+        /// <inheritdoc cref="JsonConverter.ReadJson(JsonReader, Type, object, JsonSerializer)" />>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
             JsonSerializer serializer)
         {
-            switch (reader.Value.ToString().ToLower().Trim())
+            switch (reader.Value.ToString().ToUpperInvariant().Trim())
             {
-                case "true":
+                case "TRUE":
                 case "1":
                     return true;
-                case "false":
+                case "FALSE":
                 case "0":
                     return false;
                 default:
                     return new JsonSerializer().Deserialize(reader, objectType);
             }
-
         }
 
+        /// <inheritdoc cref="JsonConverter.CanConvert(Type)" />
         public override bool CanConvert(Type objectType)
         {
             return objectType == typeof(bool);
