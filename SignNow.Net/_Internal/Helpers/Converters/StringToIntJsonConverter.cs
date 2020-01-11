fbee0ad41d51cf1531ad049219a5605f6e4d6e77
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-11 11:53:08 +0200

    MSAPI-96 | Add string-to-int json converter

---
+++ SignNow.Net/_Internal/Helpers/Converters/StringToIntJsonConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace SignNow.Net.Internal.Helpers.Converters
+{
+    /// <summary>
+    /// Converts <see cref="String"/> to <see cref="int"/> to and from JSON.
+    /// </summary>
+    internal class StringToIntJsonConverter : JsonConverter
+    {
+        /// <inheritdoc cref="JsonConverter.WriteJson" />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue((int)value);
+        }
+
+        /// <inheritdoc cref="JsonConverter.ReadJson" />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            return int.Parse(reader.Value.ToString().Trim(), NumberStyles.Integer, CultureInfo.InvariantCulture);
+        }
+
+        /// <inheritdoc cref="JsonConverter.CanConvert" />
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(int);
+        }
+    }
+}
