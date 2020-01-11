369f3a0fe66a5dc4fc604f5f2e96d0a7cf7dcc22
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-11 11:49:43 +0200

    MSAPI-96 | Add User's docblock

--- SignNow.Net/Model/User.cs
+++ SignNow.Net/Model/User.cs
@@ -8,15 +8,28 @@ namespace SignNow.Net.Model
     /// </summary>
     public class User
     {
-        [JsonProperty("active"), JsonConverter(typeof(StringToBoolJsonConverter))]
+        /// <summary>
+        /// <see cref="User"/> is active or not.
+        /// </summary>
+        [JsonProperty("active")]
+        [JsonConverter(typeof(StringToBoolJsonConverter))]
         public bool Active { get; set; }
 
+        /// <summary>
+        /// <see cref="User"/> first name.
+        /// </summary>
         [JsonProperty("first_name")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// <see cref="User"/> last name.
+        /// </summary>
         [JsonProperty("last_name")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// <see cref="User"/> email.
+        /// </summary>
         [JsonProperty("primary_email")]
         public string Email { get; set; }
     }
