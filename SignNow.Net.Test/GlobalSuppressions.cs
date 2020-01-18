481a100c16e8216e6b9fdb68fba00328e61ec878
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-18 22:23:16 +0200

    MSAPI-97 | Fix spell

--- SignNow.Net.Test/GlobalSuppressions.cs
+++ SignNow.Net.Test/GlobalSuppressions.cs
@@ -1,7 +1,7 @@
 
-// This file is used by Code Analysis to maintain SuppressMessage 
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppression either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:SignNow.Net.Test.Context.JsonCredentialProvider.GetCredential~SignNow.Net.Test.Context.CredentialModel")]
