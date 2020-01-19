9519c1e87bab29c94cc77950ba7a5efdee4f54a8
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-19 19:51:57 +0200

    MSAPI-91 | Add Field invites model, add test

---
+++ SignNow.Net.Test/UnitTests/Models/FieldInvitesTest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using SignNow.Net.Model;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FieldInvitesTest
+    {
+        [DataTestMethod]
+        [DataRow("all", FieldInvitesStatus.All, DisplayName = "Status: all")]
+        [DataRow("created", FieldInvitesStatus.Created, DisplayName = "Status: created")]
+        [DataRow("declined", FieldInvitesStatus.Declined, DisplayName = "Status: declined")]
+        [DataRow("fulfilled", FieldInvitesStatus.Fulfilled, DisplayName = "Status: fulfilled")]
+        [DataRow("pending", FieldInvitesStatus.Pending, DisplayName = "Status: pending")]
+        [DataRow("skipped", FieldInvitesStatus.Skipped, DisplayName = "Status: skipped")]
+        public void ShouldDeserializeFromJson(string status, Enum expected)
+        {
+            var json = $@"{{
+                'id': 'a09b26test07ce70228afe6290f4445700b6f349',
+                'status': '{status}',
+                'role': 'Signer 1',
+                'email': 'signer1@signnow.com',
+                'created': '1579451165',
+                'updated': '1579451165',
+                'expiration_time': '1582043165'
+            }}";
+
+            var fieldInvite = JsonConvert.DeserializeObject<FieldInvites>(json);
+
+            Assert.AreEqual("a09b26test07ce70228afe6290f4445700b6f349", fieldInvite.Id);
+            Assert.AreEqual(expected, fieldInvite.Status);
+            Assert.AreEqual("Signer 1", fieldInvite.RoleName);
+            Assert.AreEqual("signer1@signnow.com", fieldInvite.Email);
+            Assert.AreEqual("2020-01-19 16:26:05Z", fieldInvite.Created.ToString("u", CultureInfo.CurrentCulture));
+            Assert.AreEqual("2020-01-19 16:26:05Z", fieldInvite.Updated.ToString("u", CultureInfo.CurrentCulture));
+            Assert.AreEqual("2020-02-18 16:26:05Z", fieldInvite.ExpiredOn.ToString("u", CultureInfo.CurrentCulture));
+        }
+    }
+}
