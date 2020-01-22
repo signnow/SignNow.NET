1c74842e87f3d5157fbc4524881b8dd6ad7792a3
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-01-22 16:22:36 +0200

    MSAPI-101 | Add signers full options

---
+++ SignNow.Net.Test/UnitTests/Models/Internal/RoleContentTest.cs
@@ -0,0 +1,130 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using SignNow.Net.Model;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RoleContentTest
+    {
+        private Role role { get; set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            role = new Role
+            {
+                Id = "12345abc",
+                Name = "Signer 1",
+                SigningOrder = 1
+            };
+        }
+
+        [TestMethod]
+        public void ShouldCreateRoleContentWithPasswordAccess()
+        {
+            var content = new SignerOptions(role, "test@email.com")
+                {
+                    ExpirationDays = 14,
+                    RemindAfterDays = 7
+                }
+                .SetProtectionByPassword("***secret***");
+
+            var json = JsonConvert.DeserializeObject(@"{
+                'email': 'test@email.com',
+                'role': 'Signer 1',
+                'role_id': '12345abc',
+                'order': 1,
+                'authentication_type': 'password',
+                'password': '***secret***',
+                'expiration_days': 14,
+                'reminder': 7
+            }");
+
+            var actual = JsonConvert.SerializeObject(content, Formatting.Indented);
+            var expected = JsonConvert.SerializeObject(json, Formatting.Indented);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldCreateRoleContentWithPhoneAccess()
+        {
+            var content = new SignerOptions(role, "test@email.com")
+                {
+                    ExpirationDays = 7
+                }
+                .SetProtectionByPhoneCall("800 831-2050");
+
+            var json = JsonConvert.DeserializeObject(@"{
+                'email': 'test@email.com',
+                'role': 'Signer 1',
+                'role_id': '12345abc',
+                'order': 1,
+                'authentication_type': 'phone_call',
+                'phone': '800 831-2050',
+                'expiration_days': 7
+            }");
+
+            var actual = JsonConvert.SerializeObject(content, Formatting.Indented);
+            var expected = JsonConvert.SerializeObject(json, Formatting.Indented);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldCreateRoleContentWithSmsAccess()
+        {
+            var content = new SignerOptions(role, "test@email.com")
+                {
+                    RemindAfterDays = 1
+                }
+                .SetProtectionBySms("800 831-2050");
+
+            var json = JsonConvert.DeserializeObject(@"{
+                'email': 'test@email.com',
+                'role': 'Signer 1',
+                'role_id': '12345abc',
+                'order': 1,
+                'authentication_type': 'sms',
+                'phone': '800 831-2050',
+                'reminder': 1
+            }");
+
+            var actual = JsonConvert.SerializeObject(content, Formatting.Indented);
+            var expected = JsonConvert.SerializeObject(json, Formatting.Indented);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldSetOnlyOneProtection()
+        {
+            var content = new SignerOptions(role, "test@email.com")
+                {
+                    ExpirationDays = 5,
+                    RemindAfterDays = 1
+                };
+
+            content.SetProtectionBySms("800 831-2050");
+            Assert.AreEqual("800 831-2050", content.Phone);
+            Assert.AreEqual("sms", content.AuthenticationType);
+            Assert.IsNull(content.Password);
+
+            content.SetProtectionByPassword("secret");
+            Assert.AreEqual("password", content.AuthenticationType);
+            Assert.AreEqual("secret", content.Password);
+            Assert.IsNull(content.Phone);
+
+            content.SetProtectionByPhoneCall("800 831-2050");
+            Assert.AreEqual("800 831-2050", content.Phone);
+            Assert.AreEqual("phone_call", content.AuthenticationType);
+            Assert.IsNull(content.Password);
+
+            content.ClearSignerAccessProtection();
+            Assert.IsNull(content.Password);
+            Assert.IsNull(content.Phone);
+            Assert.IsNull(content.AuthenticationType);
+        }
+    }
+}
---
+++ SignNow.Net/Model/SignerOptions.cs
@@ -0,0 +1,140 @@
+using Newtonsoft.Json;
+using SignNow.Net.Internal.Helpers;
+
+namespace SignNow.Net.Model
+{
+    /// <summary>
+    /// Represents optional properties for Signer.
+    /// </summary>
+    public class SignerOptions
+    {
+        private string AuthType { get; set; }
+
+        private Role SignerRole { get; set; }
+
+        /// <summary>
+        /// Represents the email address of the signer.
+        /// </summary>
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Represents the name of the role the signer will be required to fulfill.
+        /// </summary>
+        [JsonProperty("role")]
+        public string RoleName => SignerRole.Name;
+
+        /// <summary>
+        /// Leave empty state “” for a document with one role,
+        /// or get all role IDs from Document and assign a signer to the specific `role_id`
+        /// </summary>
+        [JsonProperty("role_id")]
+        public string RoleId => SignerRole.Id;
+
+        /// <summary>
+        /// The order of signing, or Signing step. When there are multiple signers,
+        /// preset who signs the document first, who’s in the second group etc.
+        /// Each group - one signing step. For a freeform invite this equals “1”
+        /// </summary>
+        [JsonProperty("order")]
+        public int SigningOrder => SignerRole.SigningOrder;
+
+        /// <summary>
+        /// Authentication type for case, when password used to open the Document.
+        /// </summary>
+        [JsonProperty("authentication_type", NullValueHandling = NullValueHandling.Ignore)]
+        public string AuthenticationType => AuthType;
+
+        /// <summary>
+        /// Password will be required from signers when they open the document.
+        /// </summary>
+        [JsonProperty("password", NullValueHandling = NullValueHandling.Ignore)]
+        public string Password { get; private set; }
+
+        [JsonProperty("phone", NullValueHandling = NullValueHandling.Ignore)]
+        public string Phone { get; private set; }
+
+        /// <summary>
+        /// In how many days this invite expires.
+        /// </summary>
+        [JsonProperty("expiration_days", NullValueHandling = NullValueHandling.Ignore)]
+        public int? ExpirationDays { get; set; }
+
+        /// <summary>
+        /// In how many days will another email be sent to remind of a signature invite.
+        /// </summary>
+        [JsonProperty("reminder",NullValueHandling = NullValueHandling.Ignore)]
+        public int? RemindAfterDays { get; set; }
+
+        public SignerOptions(Role role, string email)
+        {
+            Guard.ArgumentNotNull(role, nameof(role));
+            SignerRole = role;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Set <see cref="Password"/> which will be required from signers when they open the document.
+        /// </summary>
+        /// <param name="password">Password for signer to open the document.</param>
+        /// <returns><see cref="SignerOptions"/></returns>
+        public SignerOptions SetProtectionByPassword(string password)
+        {
+            if (!string.IsNullOrEmpty(password))
+            {
+                Password = password;
+                Phone = null;
+                AuthType = "password";
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Set <see cref="Phone"/> number to authorize signer when they open the document via phone call.
+        /// </summary>
+        /// <param name="phone">Phone number.</param>
+        /// <returns><see cref="SignerOptions"/></returns>
+        public SignerOptions SetProtectionByPhoneCall(string phone)
+        {
+            if (!string.IsNullOrEmpty(phone))
+            {
+                Password = null;
+                Phone = phone;
+                AuthType = "phone_call";
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Set <see cref="Phone"/> number to authorize signer when they open the document via sms code.
+        /// </summary>
+        /// <param name="phone">Phone number.</param>
+        /// <returns><see cref="SignerOptions"/></returns>
+        public SignerOptions SetProtectionBySms(string phone)
+        {
+            if (!string.IsNullOrEmpty(phone))
+            {
+                Password = null;
+                Phone = phone;
+                AuthType = "sms";
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Clear Signer access protection options.
+        /// </summary>
+        /// <returns></returns>
+        public SignerOptions ClearSignerAccessProtection()
+        {
+            AuthType = null;
+            Password = null;
+            Phone = null;
+
+            return this;
+        }
+    }
+}
