cd0f6fa87a07531a6df5d12f278d92cf2a048f37
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-02-13 11:55:19 +0200

    MSAPI-89 | Add Guard tests

---
+++ SignNow.Net.Test/UnitTests/Helpers/GuardTest.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SignNow.Net.Internal.Helpers;
+using SignNow.Net.Model;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GuardTest
+    {
+        [TestMethod]
+        public void ShouldGuardArgumentNullException()
+        {
+            Object nullableObj = null;
+
+            var exception = Assert.ThrowsException<ArgumentNullException>(
+                () => Guard.ArgumentNotNull(nullableObj, nameof(nullableObj)));
+
+            Assert.AreEqual($"Value cannot be null.\nParameter name: nullableObj", exception.Message);
+        }
+
+        [TestMethod]
+        public void ShouldGuardPropertyNullException()
+        {
+            var doc = new SignNowDocument();
+
+            var exception = Assert.ThrowsException<ArgumentException>(
+                () => Guard.PropertyNotNull(doc.Id, "Document ID cannot be null."));
+
+            Assert.AreEqual("Document ID cannot be null.", exception.Message);
+        }
+    }
+}
