2c75d27502bf6eacd8368d4cf529d00a5bec4cba
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-03-07 22:38:41 +0200

    Fix code style

--- SignNow.Net.Test/Constants/ErrorMessages.cs
+++ SignNow.Net.Test/Constants/ErrorMessages.cs
@@ -7,21 +7,21 @@ namespace SignNow.Net.Test.Constants
     /// </summary>
     public static class ErrorMessages
     {
-        public const string InvalidFileType = "Invalid file type.";
-        public const string CannotCreateSigningLinksOnDocumentsWithNoFields = "Cannot create signing links on documents with no fields";
-        public const string TheDocumentIdShouldHave40Characters = "The document id should have exactly 40 characters.";
-        public const string BadRequest = "Bad Request";
+        public static readonly string InvalidFileType = "Invalid file type.";
+        public static readonly string CannotCreateSigningLinksOnDocumentsWithNoFields = "Cannot create signing links on documents with no fields";
+        public static readonly string TheDocumentIdShouldHave40Characters = "The document id should have exactly 40 characters.";
+        public static readonly string BadRequest = "Bad Request";
 
         /// <summary>
         /// Invalid format of ID {param}...
         /// </summary>
-        public const string InvalidFormatOfId =
+        public static readonly string InvalidFormatOfId =
             "Invalid format of ID <{0}>. The required format: 40 characters long, case-sensitive, letters and numbers, underscore allowed.";
 
         /// <summary>
         /// Invalid format of email {param}...
         /// </summary>
-        public const string InvalidFormatOfEmail =
+        public static readonly string InvalidFormatOfEmail =
             "Invalid format of email <{0}>. The required format: valid email address (e.g john+1@gmail.com or john123@gmail.com).";
 
         /// <summary>
