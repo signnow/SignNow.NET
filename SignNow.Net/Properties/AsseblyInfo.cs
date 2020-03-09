0aa664fde21b45bd885af0bbf2ce8e3ee3615ec5
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-03-09 21:17:27 +0200

    Add Moq framework

--- SignNow.Net/Properties/AsseblyInfo.cs
+++ SignNow.Net/Properties/AsseblyInfo.cs
@@ -3,4 +3,6 @@ using System.Runtime.CompilerServices;
 #if DEBUG
 [assembly: InternalsVisibleTo("SignNow.Net.Test")]
 [assembly: InternalsVisibleTo("Bogus")]
+// Requires for Moq ProxyBuilder
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 #endif
