fc5b0be9b45475f4479cb5e3111fa8a62d95ccbb
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-10-04 22:04:51 +0300

    Add merge request test

---
+++ SignNow.Net.Test/UnitTests/Requests/MergeDocumentRequestTest.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SignNow.Net.Internal.Requests;
+using SignNow.Net.Test;
+using SignNow.Net.Test.FakeModels;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MergeDocumentRequestTest : SignNowTestBase
+    {
+        [TestMethod]
+        public void SerializeModel()
+        {
+            var mergedName = "Test_Merged_Document_Name";
+            var documents = new SignNowDocumentFaker().Generate(5);
+
+            var request = new MergeDocumentRequest
+            {
+                Name = mergedName
+            };
+            request.AddDocuments(documents);
+
+            var serialized = SerializeToJsonFormatted(request);
+            var requestJ = DeserializeFromJson<MergeDocumentRequest>(serialized);
+
+            Assert.AreEqual(mergedName, requestJ.Name);
+            Assert.AreEqual(5, requestJ.DocumentIds.Count);
+            Assert.AreEqual(documents[1].Id, requestJ.DocumentIds[1]);
+        }
+
+        [TestMethod]
+        public void CreatesJsonContext()
+        {
+            var documents = new SignNowDocumentFaker().Generate(10);
+            var request = new MergeDocumentRequest
+            {
+                Name = "mergedName"
+            };
+            request.AddDocuments(documents);
+
+            var JsonHttpContext = request.GetHttpContent();
+            var jsonBody = JsonHttpContext.ReadAsStringAsync().Result;
+
+            Assert.AreEqual("application/json", JsonHttpContext.Headers.ContentType.MediaType);
+            Assert.IsTrue(jsonBody.Contains("mergedName"));
+            Assert.IsTrue(jsonBody.Contains(documents[1].Id));
+        }
+    }
+}
