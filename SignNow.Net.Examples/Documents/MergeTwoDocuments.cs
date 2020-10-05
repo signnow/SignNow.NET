58ff0a69089753cf2810c6815f1ae36422e4af4a
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-10-05 17:51:15 +0300

    Add code samples

--- SignNow.Net.Examples/Documents/MergeTwoDocuments.cs
+++ SignNow.Net.Examples/Documents/MergeTwoDocuments.cs
@@ -1,4 +1,25 @@
-$HEADER$namespace $NAMESPACE$
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SignNow.Net.Model;
+
+namespace SignNow.Net.Examples.Documents
 {
-  public class $CLASS$ {$END$}
+    public static partial class DocumentExamples
+    {
+        /// <summary>
+        /// Merge two documents into one final document
+        /// </summary>
+        /// <param name="documentName">New Document name with extension</param>
+        /// <param name="documentsList">List of the documents to be merged</param>
+        /// <param name="token">Access token</param>
+        /// <returns></returns>
+        public static async Task<DownloadDocumentResponse> MergeTwoDocuments(string documentName, IEnumerable<SignNowDocument> documentsList, Token token)
+        {
+            var signNowContext = new SignNowContext(token);
+
+            return await signNowContext.Documents
+                .MergeDocumentsAsync(documentName, documentsList)
+                .ConfigureAwait(false);
+        }
+    }
 }
