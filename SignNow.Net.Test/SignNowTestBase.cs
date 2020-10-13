4cd8b830773eb73d4d3d868ebd5aef1d9d2143d5
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-10-13 19:18:06 +0300

    Add signnow client mock

--- SignNow.Net.Test/SignNowTestBase.cs
+++ SignNow.Net.Test/SignNowTestBase.cs
@@ -1,5 +1,13 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using SignNow.Net.Interfaces;
+using SignNow.Net.Internal.Service;
 
 namespace SignNow.Net.Test
 {
@@ -18,5 +26,29 @@ namespace SignNow.Net.Test
 
         protected static string PdfFilePath => Path.Combine($"{BaseTestDataPath}Documents", PdfFileName);
         protected static string TxtFilePath => Path.Combine($"{BaseTestDataPath}Documents", TxtFileName);
+
+        /// <summary>
+        /// Create Mock for HttpClient
+        /// </summary>
+        /// <param name="jsonResponse">Json response that you want to be returned</param>
+        /// <param name="code">Http status that you expect</param>
+        /// <returns></returns>
+        protected ISignNowClient SignNowClientMock(string jsonResponse, HttpStatusCode code = HttpStatusCode.OK)
+        {
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = code,
+                    Content = new StringContent(jsonResponse)
+                })
+                .Verifiable();
+
+            // use real http client with mocked handler here
+            return new SignNowClient(new HttpClient(handlerMock.Object, false));
+        }
     }
 }
