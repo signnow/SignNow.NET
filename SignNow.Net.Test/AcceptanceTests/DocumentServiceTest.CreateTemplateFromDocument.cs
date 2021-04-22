bcd3b7c92a829728098ce1305799597b24c03774
SashaGolovach <gsancho0001@gmail.com>
2021-04-22 16:26:55 +0300

    Update DocumentServiceTest.CreateTemplateFromDocument.cs

--- SignNow.Net.Test/AcceptanceTests/DocumentServiceTest.CreateTemplateFromDocument.cs
+++ SignNow.Net.Test/AcceptanceTests/DocumentServiceTest.CreateTemplateFromDocument.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SignNow.Net.Model.Requests;
 using UnitTests;
@@ -7,14 +8,14 @@ namespace AcceptanceTests
     public partial class DocumentServiceTest : AuthorizedApiTestBase
     {
         [TestMethod]
-        public void CreateTemplateFromDocumentSuccessfully()
+        public async Task CreateTemplateFromDocumentSuccessfully()
         {
             var response =
-                SignNowTestContext.Documents.CreateTemplateFromDocumentAsync(new CreateTemplateFromDocumentRequest
+                await SignNowTestContext.Documents.CreateTemplateFromDocumentAsync(new CreateTemplateFromDocumentRequest
                 {
                     DocumentId = TestPdfDocumentId,
                     TemplateName = "test template name"
-                }).Result;
+                });
 
             Assert.IsNotNull(response.Id);
         }
