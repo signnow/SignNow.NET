3019e81a7331b7619f4fa661b49b2efbb7a8014d
AlexNDRmac <AlexNDR.mac@gmail.com>
2021-04-26 14:55:25 +0300

    Change Interface docblock, move request to common place

--- SignNow.Net/Model/Responses/CreateTemplateFromDocumentResponse.cs
+++ SignNow.Net/Model/Responses/CreateTemplateFromDocumentResponse.cs
@@ -1,6 +1,6 @@
 using Newtonsoft.Json;
 
-namespace SignNow.Net.Model
+namespace SignNow.Net.Model.Responses
 {
     /// <summary>
     /// Represents response from SignNow API for Create Template from Document request.
