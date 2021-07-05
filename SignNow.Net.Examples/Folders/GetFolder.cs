9c36741affcdeaf1fec47bf4b01d70a6aa0d57c3
AlexNDRmac <alexndr.mac@gmail.com>
2021-07-05 08:53:54 +0300

    Update readme

--- SignNow.Net.Examples/Folders/GetFolder.cs
+++ SignNow.Net.Examples/Folders/GetFolder.cs
@@ -6,6 +6,13 @@ namespace SignNow.Net.Examples.Folders
 {
     public static partial class FolderExamples
     {
+        /// <summary>
+        /// Get all details of a specific folder including a list of all documents in that folder.
+        /// </summary>
+        /// <param name="folderId">ID of the folder to get details of</param>
+        /// <param name="options">Options like: sort, filter, limit, etc...</param>
+        /// <param name="token">Access token</param>
+        /// <returns></returns>
         public static async Task<SignNowFolders> GetFolder(string folderId, GetFolderOptions options, Token token)
         {
             var signNowContext = new SignNowContext(token);
