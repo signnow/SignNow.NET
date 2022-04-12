d955cc407404452cdce2c02a14359f3c0d0b1752
AlexNDRmac <alexndr.mac@gmail.com>
2022-04-12 19:35:53 +0300

    Create initial Event Subscription Interface

---
+++ SignNow.Net/Interfaces/IEventSubscriptionService.cs
@@ -0,0 +1,41 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignNow.Net.Interfaces
+{
+    /// <summary>
+    /// Interface for any operation with Events.
+    /// Can be used for Create, Update, Delete and Get information about
+    /// all subscriptions to events made with a specific application.
+    /// </summary>
+    public interface IEventSubscriptionService
+    {
+        /// <summary>
+        /// Allows to subscribe an external service (callback_url) to a specific Event of User or Document.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns></returns>
+        Task CreateEventSubscriptionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Gets information about all subscriptions to Events made with a specific application.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns></returns>
+        Task GetEventSubscriptionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Allows changing an existing Event subscription.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns></returns>
+        Task UpdateEventSubscriptionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Unsubscribes an external service (callback_url) from specific Events of User or Document
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns></returns>
+        Task DeleteEventSubscriptionAsync(CancellationToken cancellationToken = default);
+    }
+}
