933e4b1b8ed214f87ed836d0fee5fa6da3e9fb0e
AlexNDRmac <alexndr.mac@gmail.com>
2022-04-13 22:52:13 +0300

    Fix Event subscription model

--- SignNow.Net/Model/EventSubscription.cs
+++ SignNow.Net/Model/EventSubscription.cs
@@ -12,11 +12,23 @@ namespace SignNow.Net.Model
         [JsonProperty("id")]
         public string Id { get; set; }
 
+        [JsonProperty("event")]
         public string Event { get; set; }
+
+        [JsonProperty("entity_id")]
         public int EntityId { get; set; }
+
+        [JsonProperty("entity_unique_id")]
         public string EntityUid { get; set; }
+
+        [JsonProperty("action")]
         public string Action { get; set; }
-        public string JsonAttributes { get; set; }
+
+        [JsonProperty("json_attributes")]
+        public EventAttributes JsonAttributes { get; set; }
+
+        [JsonProperty("application_name")]
+        public string ApplicationName { get; set; }
 
         /// <summary>
         /// Timestamp document was created.
@@ -24,5 +36,18 @@ namespace SignNow.Net.Model
         [JsonProperty("created")]
         [JsonConverter(typeof(UnixTimeStampJsonConverter))]
         public DateTime Created { get; set; }
+    }
+
+    public class EventAttributes
+    {
+        [JsonProperty("use_tls_12")]
+        public bool UseTls12 { get; set; }
+
+        [JsonProperty("docid_queryparam")]
+        public bool DocIdQueryParam { get; set; }
+
+        [JsonProperty("callback_url")]
+        [JsonConverter(typeof(StringToUriJsonConverter))]
+        public Uri CallbackUrl { get; set; }
     }
 }
