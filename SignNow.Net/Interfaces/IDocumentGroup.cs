c3370244faf7ae3c6260d72f209e5214c35f1212
AlexNDRmac <alexndr.mac@gmail.com>
2024-10-17 20:53:09 +0300

    Add interface for doc group

---
+++ SignNow.Net/Interfaces/IDocumentGroup.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SignNow.Net.Model;
+
+namespace SignNow.Net.Interfaces
+{
+    /// <summary>
+    /// Interface for any operations with a Document Groups in signNow
+    /// can be used to create, rename, delete, move a document group etc.
+    /// </summary>
+    public interface IDocumentGroup
+    {
+        /// <summary>
+        /// Creates a document group from a list of document ids
+        /// </summary>
+        /// <param name="groupName">Name for the Document Group</param>
+        /// <param name="documents">The list of the documents to create the document group.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns></returns>
+        Task CreateDocumentGroupAsync(string groupName, IEnumerable<SignNowDocument> documents, CancellationToken cancellationToken = default);
+    }
+}
