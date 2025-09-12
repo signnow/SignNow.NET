d44e7e5b5c2da2784338718deea81e1c1bcad4f2
raman-liavitski-airslate <raman.liavitski@airslate.com>
2025-09-12 15:08:55 +0200

    [SNAPI-19550] remove test for getter/setter properties

--- SignNow.Net.Test/UnitTests/Models/TokenTest.cs
+++ SignNow.Net.Test/UnitTests/Models/TokenTest.cs
@@ -8,22 +8,6 @@ namespace UnitTests.Models
     [TestClass]
     public class TokenTest
     {
-        [TestMethod]
-        public void ShouldDeserializeFromJson()
-        {
-            var fakeToken = new TokenFaker().Generate();
-            var fakeJson = TestUtils.SerializeToJsonFormatted(fakeToken);
-
-            var actual = TestUtils.DeserializeFromJson<Token>(fakeJson);
-            var actualJson = TestUtils.SerializeToJsonFormatted(actual);
-
-            Assert.AreEqual(fakeJson, actualJson);
-            Assert.AreEqual(fakeToken.AccessToken, actual.AccessToken);
-            Assert.AreEqual(fakeToken.ExpiresIn, actual.ExpiresIn);
-            Assert.AreEqual(fakeToken.Scope, actual.Scope);
-            Assert.AreEqual(fakeToken.TokenType, actual.TokenType);
-            Assert.AreNotEqual(fakeToken.AppToken, actual.AppToken);
-        }
 
         [TestMethod]
         public void ShouldGetAuthorizationHeaderValue()
