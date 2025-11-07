2bbb185625f27e20fdfb62c0298c034e104a5c24
Andrii <yudaichev.andrii@pdffiller.team>
2025-11-07 20:32:40 +0200

    SNINT-1905-get-list-of-event | Add EventSubscriptionListTest AcceptanceTest

--- SignNow.Net.Test/AcceptanceTests/EventSubscriptionListTest.cs
+++ SignNow.Net.Test/AcceptanceTests/EventSubscriptionListTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,125 +13,34 @@ namespace AcceptanceTests
     public class EventSubscriptionListTest : AuthorizedApiTestBase
     {
         [TestMethod]
-        public async Task GetEventSubscriptionsListAsync_WithBasicOptions()
+        public async Task GetEventSubscriptionsListAsync_WithFilters()
         {
-            // Act - Get event subscriptions with basic options
+            await SignNowTestContext.Events.CreateEventSubscriptionAsync(
+                new CreateEventSubscription(EventType.DocumentFreeformSigned, TestPdfDocumentId, new Uri("https://docs.signnow.com"))
+            );
+
             var options = new GetEventSubscriptionsListOptions
             {
                 Page = 1,
-                IncludeEventCount = true
+                PerPage = 5,
+                EventTypeFilter = EventTypeFilter.In(EventType.DocumentFreeformSigned),
+                CallbackUrlFilter = CallbackUrlFilter.Like("docs.signnow"),
+                EntityIdFilter = EntityIdFilter.Like(TestPdfDocumentId),
+                SortByCreated = SortOrder.Descending
             };
-
             var response = await SignNowTestContext.Events
-                .GetEventSubscriptionsListAsync(options)
-                .ConfigureAwait(false);
+                .GetEventSubscriptionsListAsync(options);
 
-            // Assert
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Data);
-            Assert.IsNotNull(response.Meta);
-            Assert.IsNotNull(response.Meta.Pagination);
-            Assert.IsTrue(response.Meta.Pagination.Total >= 0);
-        }
+            Assert.AreEqual(5, response.Meta.Pagination.PerPage);
+            Assert.IsTrue(response.Data.Count > 0);
+            var subscription = response.Data.First();
+            Assert.AreEqual(EventType.DocumentFreeformSigned, subscription.Event);
+            Assert.AreEqual(TestPdfDocumentId, subscription.EntityUid);
+            Assert.AreEqual("post", subscription.RequestMethod);
+            Assert.AreEqual(true, subscription.Active);
 
-        [TestMethod]
-        public async Task GetEventSubscriptionsListAsync_WithSorting()
-        {
-            // Act - Get event subscriptions with sorting
-            var options = new GetEventSubscriptionsListOptions
-            {
-                SortByCreated = SortOrder.Descending,
-                Page = 1
-            };
-
-            var response = await SignNowTestContext.Events
-                .GetEventSubscriptionsListAsync(options)
-                .ConfigureAwait(false);
-
-            // Assert
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Data);
-            Assert.IsNotNull(response.Meta);
-            
-            // If there are multiple subscriptions, verify they are sorted by creation date descending
-            if (response.Data.Count > 1)
-            {
-                var sortedData = response.Data.OrderByDescending(x => x.Created).ToList();
-                for (int i = 0; i < response.Data.Count; i++)
-                {
-                    Assert.AreEqual(sortedData[i].Id, response.Data[i].Id, 
-                        "Event subscriptions should be sorted by creation date in descending order");
-                }
-            }
-        }
-
-        [TestMethod]
-        public async Task GetEventSubscriptionsListAsync_WithoutOptions()
-        {
-            // Act - Get event subscriptions without any options
-            var response = await SignNowTestContext.Events
-                .GetEventSubscriptionsListAsync()
-                .ConfigureAwait(false);
-
-            // Assert
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Data);
-            Assert.IsNotNull(response.Meta);
-            Assert.IsNotNull(response.Meta.Pagination);
-            Assert.IsTrue(response.Meta.Pagination.Total >= 0);
-        }
-
-        [TestMethod]
-        public async Task GetEventSubscriptionsListAsync_WithEventTypeFilter()
-        {
-            // Act - Get event subscriptions filtered by event type
-            var options = new GetEventSubscriptionsListOptions
-            {
-                EventTypeFilter = new[] { EventType.DocumentComplete, EventType.DocumentUpdate },
-                Page = 1
-            };
-
-            var response = await SignNowTestContext.Events
-                .GetEventSubscriptionsListAsync(options)
-                .ConfigureAwait(false);
-
-            // Assert
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Data);
-            
-            // If there are results, verify they match the filter
-            foreach (var subscription in response.Data)
-            {
-                Assert.IsTrue(
-                    subscription.Event == EventType.DocumentComplete || 
-                    subscription.Event == EventType.DocumentUpdate,
-                    $"Event subscription should have event type DocumentComplete or DocumentUpdate, but was {subscription.Event}");
-            }
-        }
-
-        [TestMethod]
-        public async Task GetEventSubscriptionsListAsync_CompareWithOriginalMethod()
-        {
-            // Act - Get results from both methods
-            var originalResponse = await SignNowTestContext.Events
-                .GetEventSubscriptionsAsync(new PagePaginationOptions { Page = 1 })
-                .ConfigureAwait(false);
-
-            var newResponse = await SignNowTestContext.Events
-                .GetEventSubscriptionsListAsync(new GetEventSubscriptionsListOptions { Page = 1 })
-                .ConfigureAwait(false);
-
-            // Assert - Both should return data (structure might be similar for basic queries)
-            Assert.IsNotNull(originalResponse);
-            Assert.IsNotNull(newResponse);
-            Assert.IsNotNull(originalResponse.Data);
-            Assert.IsNotNull(newResponse.Data);
-            Assert.IsNotNull(originalResponse.Meta);
-            Assert.IsNotNull(newResponse.Meta);
-
-            // Both methods should handle pagination
-            Assert.IsNotNull(originalResponse.Meta.Pagination);
-            Assert.IsNotNull(newResponse.Meta.Pagination);
+            // todo: update to v2 DeleteEventSubscription
+            await SignNowTestContext.Events.DeleteEventSubscriptionAsync(response.Data.First().Id);
         }
     }
 }
